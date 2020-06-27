51e9f876d027133d0083584597984c7be922eb48
kmh339 <kmh339@gmail.com>
2020-06-27 23:29:23 +0900

    Splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,19 +21,44 @@ class MyApp extends StatelessWidget {
 }
 
 class SplashScreen extends StatefulWidget {
-  
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('splash'),
-      ),
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20),
+            alignment: Alignment.center,
+            height: 100,
+            width: MediaQuery.of(context).size.width,
+            child: ColorizeAnimatedTextKit(
+                repeatForever: true,
+                pause: Duration(milliseconds: 0),
+                speed: Duration(milliseconds: 400),
+                colors: [
+                  Colors.lightBlueAccent,
+                  Colors.blue,
+                  Colors.greenAccent,
+                  Colors.blue,
+                  Colors.lightBlueAccent,
+                ],
+                text: ["바른연애생활"],
+                textStyle: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.white),
+                textAlign: TextAlign.start,
+                alignment: AlignmentDirectional.topStart // or Alignment.topLeft
+                ),
+          ),
+          const SpinKitFadingCube(color: Colors.lightBlueAccent, size: 30,),
+
+        ],
+      )),
     );
   }
 }
